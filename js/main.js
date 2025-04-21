ce50854d6c5a8e8d66942d3e23441e1cdbd5f09d
ArshamDavariShaslamzari <arshdav1376@gmail.com>
2025-04-21 19:52:53 +0200

    loading pictures in browswer but still has problem

--- js/main.js
+++ js/main.js
@@ -29,18 +29,42 @@ for (let { name, href, showOn } of navItems) {
   }
 }
 
-// loading data from JSON (API)
-
 document.body.prepend(nav);
 
+// loading data from JSON (API)
 let covers = [];
-
+const comingSoonElement = document.getElementById("coming-soon");
 async function loadData() {
   const coverResponse = await fetch("data/covers.json");
   const coverJSON = await coverResponse.json();
   covers = coverJSON.covers;
 
-  console.log(coverJSON);
+  renderContent();
+}
+
+function getCoverByID(id) {
+  return covers.find((cover) => cover.id === id);
+}
+
+function createCoverElement(cover, coverId) {
+  const coverElement = document.createElement("figure");
+  coverElement.classList.add("cover");
+
+  const imageElement = document.createElement("img");
+  imageElement.src = cover.coverImage;
+  coverElement.appendChild(imageElement);
+
+  return coverElement;
+}
+
+function renderContent() {
+  comingSoonElement.innerHTML = "";
+
+  for (let cover of covers) {
+    const coverId = getCoverByID(cover.id);
+    const coverElement = createCoverElement(cover, coverId);
+    comingSoonElement.appendChild(coverElement);
+  }
 }
 
 loadData();
